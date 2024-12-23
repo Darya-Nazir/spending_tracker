72a1c7c1c1a9a1b85a0a93cb33178d5d129b93fa
Darya Nazir <somebodyintheway@gmail.com>
2024-12-23 13:36:31 +0500

    Create base class CratingCard

---
+++ client/scripts/base-class/creating-card.js
@@ -0,0 +1,55 @@
+import {Http} from "../services/http.js";
+
+export class CreatingCard {
+    constructor(navigateTo, apiUrl, redirectPath) {
+        this.navigateToPath = navigateTo;
+        this.apiUrl = apiUrl;
+        this.redirectPath = redirectPath;
+    }
+
+    init() {
+        this.addCategoryConfirmListener();
+        this.cancelCategoryButtonListener();
+    }
+
+    addCategoryConfirmListener() {
+        document.getElementById('create').addEventListener('click',
+            (event) => this.handleCategoryCreation(event));
+    }
+
+    async handleCategoryCreation(event) {
+        event.preventDefault(); // Предотвращаем отправку формы
+
+        const categoryName = this.getCategoryName();
+        if (!categoryName) {
+            alert('Введите название категории!');
+            return;
+        }
+
+        try {
+            await Http.request(this.apiUrl, 'POST', { title: categoryName });
+            this.navigateToPath(this.redirectPath); // Успешное добавление
+        } catch (error) {
+            this.handleCategoryCreationError(error);
+        }
+    }
+
+    getCategoryName() {
+        const input = document.querySelector('.form-control');
+        return input.value.trim();
+    }
+
+    handleCategoryCreationError(error) {
+        console.error('Category addition: ', error);
+        alert('Не удалось добавить категорию, попробуйте еще раз. ' +
+            'Если категория уже существует, она не будет добавлена.');
+    }
+
+    cancelCategoryButtonListener() {
+        document.getElementById('cancel').addEventListener('click', (event) => {
+            event.preventDefault();
+            this.navigateToPath(this.redirectPath);
+        });
+    }
+}
+
--- client/src/components/create_cost.js
+++ client/src/components/create_cost.js
@@ -1,59 +1,8 @@
-import {Auth} from "../../scripts/services/auth.js";
-import {Http} from "../../scripts/services/http.js";
+import {CreatingCard} from "../../scripts/base-class/creating-card.js";
 
-export class NewCost {
+export class NewCost extends CreatingCard {
     constructor(navigateTo) {
-        this.navigateToPath = navigateTo;
-    }
-
-    init() {
-        this.addCategoryConfirmListener();
-        this.cancelCategoryButtonListener();
-    }
-
-    addCategoryConfirmListener() {
-        document.getElementById('create').addEventListener('click',
-            (event) => this.handleCategoryCreation(event));
-    }
-
-    async handleCategoryCreation(event) {
-        event.preventDefault(); // Предотвращаем отправку формы
-
-        const categoryName = this.getCategoryName();
-        if (!categoryName) {
-            alert('Введите название категории!');
-            return;
-        }
-
-        try {
-            const response = await Http.request(
-                'http://localhost:3000/api/categories/expense',
-                'POST',
-                { title: categoryName }
-            );
-
-            this.navigateToPath('/costs'); // Успешное добавление
-        } catch (error) {
-            this.handleCategoryCreationError(error);
-        }
-    }
-
-    getCategoryName() {
-        const input = document.querySelector('.form-control');
-        return input.value.trim();
-    }
-
-    handleCategoryCreationError(error) {
-        console.error('Category addition: ', error);
-        alert('Не удалось добавить категорию, попробуйте еще раз. ' +
-            'Если категория уже существует, она не будет добавлена.');
-    }
-
-    cancelCategoryButtonListener() {
-        document.getElementById('cancel').addEventListener('click', (event) => {
-            event.preventDefault();
-            this.navigateToPath('/costs');
-        });
+        super(navigateTo, 'http://localhost:3000/api/categories/expense', '/costs');
     }
 }
 
