cdd765bddf27771ad84213930982450b8eefc651
Darya Nazir <somebodyintheway@gmail.com>
2025-02-25 15:39:37 +0500

    Convert auth to TS

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,16 +1,19 @@
+import {RoutePath} from "../types/route-type";
+import {UserInfo} from "../types/user-info-type";
+
 export class Auth {
-    static navigateToPath = null;
+    public static navigateToPath: (path: RoutePath) => void;
 
     // Метод для инициализации
-    static initialize(navigateFunction) {
+    public static initialize(navigateFunction: (path: RoutePath) => void): void {
         this.navigateToPath = navigateFunction;
     }
 
-    static accessTokenKey = 'accessToken';
-    static refreshTokenKey = 'refreshToken';
-    static userInfoKey = 'userInfo';
+    public static accessTokenKey = 'accessToken';
+    public static refreshTokenKey = 'refreshToken';
+    public static userInfoKey = 'userInfo';
 
-    static async processUnauthorizedResponse() {
+    public static async processUnauthorizedResponse(): Promise<boolean > {
         const refreshToken = localStorage.getItem(this.refreshTokenKey);
         if (refreshToken) {
             const response = await fetch('http://localhost:3000/api/refresh', {
@@ -39,22 +42,22 @@ export class Auth {
         return false;
     }
 
-    static setTokens(accessToken, refreshToken) {
+    public static setTokens(accessToken: string, refreshToken: string): void {
         localStorage.setItem(this.accessTokenKey, accessToken);
         localStorage.setItem(this.refreshTokenKey, refreshToken);
     }
 
-    static removeTokens() {
+    public static removeTokens(): void {
         localStorage.removeItem(this.accessTokenKey);
         localStorage.removeItem(this.refreshTokenKey);
         localStorage.removeItem(this.userInfoKey);
     }
 
-    static setUserInfo(info) {
+    public static setUserInfo(info: UserInfo): void {
         localStorage.setItem(this.userInfoKey, JSON.stringify(info))
     }
 
-    static getUserInfo() {
+    public static getUserInfo(): any {
         const userInfo = localStorage.getItem(this.userInfoKey);
         if (userInfo) {
             return JSON.parse(userInfo);
