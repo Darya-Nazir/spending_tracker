a653041feb51c816b45daaab54f6db318ebaf4d4
Darya Nazir <somebodyintheway@gmail.com>
2025-02-26 11:38:18 +0500

    Convert unselect to TS

--- client/src/services/unselect.ts
+++ client/src/services/unselect.ts
@@ -2,25 +2,27 @@ export class Unselect {
     constructor() {
     }
 
-    init() {
+    public init(): void {
         this.removeSelect();
     }
 
-    removeSelect() {
-        const navbar = document.getElementById('navbar');
+    public removeSelect(): void {
+        const navbar: HTMLElement | null = document.getElementById('navbar');
 
         if (!navbar) {
             return;
         }
         // Удаляем класс у всех дочерних элементов
-        navbar.querySelectorAll('.bg-primary').forEach(element => {
+        navbar.querySelectorAll('.bg-primary').forEach((element: Element): void => {
             element.classList.remove('bg-primary');
             element.classList.remove('text-white');
         });
 
-        const dropdownButton = document.getElementById('dropdownMenuButton1');
+        const dropdownButton: HTMLElement | null = document.getElementById('dropdownMenuButton1');
+        if (!dropdownButton) return;
 
         dropdownButton.classList.remove('btn-primary');
         dropdownButton.classList.remove('text-white');
     }
 }
+
