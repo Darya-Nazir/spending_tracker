f7775b0760fd9b868675e109fbdbacdcca9b4db6
Darya Nazir <somebodyintheway@gmail.com>
2025-02-27 17:25:43 +0500

    Convert validation base class to TS

--- client/src/components/base-class/validation.ts
+++ client/src/components/base-class/validation.ts
@@ -1,50 +1,58 @@
+import {RoutePath} from "../../types/route-type";
+
 export class Validation {
-    constructor(navigateTo) {
+    protected form: HTMLFormElement | null;
+    protected emailInput: HTMLInputElement | null;
+    protected passwordInput: HTMLInputElement | null;
+    private emailRegex: RegExp;
+    protected navigateToPath: (path: RoutePath) => void;
+
+    constructor(navigateTo: (path: RoutePath) => void) {
         if (this.constructor === Validation) {
             throw new Error('Validation is an abstract class and cannot be instantiated directly');
         }
 
-        this.form = document.getElementById('registrationForm');
-        this.emailInput = document.getElementById('email');
-        this.passwordInput = document.getElementById('password');
+        this.form = document.getElementById('registrationForm') as HTMLFormElement;
+        this.emailInput = document.getElementById('email') as HTMLInputElement;
+        this.passwordInput = document.getElementById('password') as HTMLInputElement;
         this.emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         this.navigateToPath = navigateTo;
     }
 
-    init() {
+    protected init(): void {
         if (this.form) {
             this.initializeEventListeners();
         }
     }
 
-    initializeEventListeners() {
+    protected initializeEventListeners(): void {
         throw new Error('Method initializeEventListeners() must be implemented in derived class');
     }
 
-    validateEmail() {
-        if (!this.emailRegex.test(this.emailInput.value.trim())) {
-            this.markInputAsInvalid(this.emailInput);
+    protected validateEmail(): boolean {
+        if (!this.emailRegex.test((this.emailInput as HTMLInputElement).value.trim())) {
+            this.markInputAsInvalid(this.emailInput!);
             return false;
         }
-        this.markInputAsValid(this.emailInput);
+        this.markInputAsValid(this.emailInput!);
         return true;
     }
 
-    validatePassword(minLength = 6) {
-        if (this.passwordInput.value.length < minLength) {
-            this.markInputAsInvalid(this.passwordInput);
+    protected validatePassword(minLength = 6): boolean {
+        if ((this.passwordInput as HTMLInputElement).value.length < minLength) {
+            this.markInputAsInvalid(this.passwordInput!);
             return false;
         }
-        this.markInputAsValid(this.passwordInput);
+        this.markInputAsValid(this.passwordInput!);
         return true;
     }
 
-    markInputAsInvalid(inputElement) {
+    protected markInputAsInvalid(inputElement: HTMLInputElement) {
         inputElement.classList.remove('is-valid');
         inputElement.classList.add('is-invalid');
     }
 
-    markInputAsValid(inputElement) {
+    protected markInputAsValid(inputElement: HTMLInputElement) {
         inputElement.classList.remove('is-invalid');
         inputElement.classList.add('is-valid');
     }
