98f41388c92797bbf920778df5c2605f7d43fc40
Darya Nazir <somebodyintheway@gmail.com>
2025-03-07 18:11:06 +0500

    Correct errors in project building

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,14 +1,6 @@
-// Импортируем глобальные типы для jQuery
-/// <reference types="jquery" />
-/// <reference types="bootstrap" />
-
-// Импортируем библиотеки
 import 'jquery';
 import '@popperjs/core';
 import 'bootstrap';
-
-// Для bootstrap-datepicker может не быть типов, поэтому используем require
-// или объявляем модуль самостоятельно
 import 'bootstrap-datepicker';
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.ru.min';
 
